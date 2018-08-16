760685241f213220d7cd76e6bde5ce1985786a48
margaritov <lubo@hush.com>
2018-08-16 11:44:48 +0300

    Added lecture 1 resources and lab

---
+++ Lecture 1 - Linear Data Structures and DS Complexity/Lab/01. Data-Structures-Lab-Skeleton-CSharp/Linear-Data-Structures/Lists/ArrayList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public class ArrayList<T>
+{
+    private const int Initial_Capacity = 2;
+    private T[] items;
+    public ArrayList()
+    {
+        this.items = new T[Initial_Capacity];
+    }
+
+    public int Count { get; private set; }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index >= this.Count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return this.items[index];
+        }
+
+        set
+        {
+            if (index >= this.Count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            this.items[index] = value;
+        }
+    }
+
+
+    public void Add(T item)
+    {
+        if (this.items.Length == this.Count )
+        {
+            this.Resize();
+        }
+        this.items[Count++] = item;
+    }
+
+    private void Resize()
+    {
+        T[] copy = new T[this.items.Length * 2];
+        for (int i = 0; i < this.items.Length; i++)
+        {
+            copy[i] = this.items[i];
+        }
+        this.items = copy;
+    }
+
+    public T RemoveAt(int index)
+    {
+        if (index >= this.Count || index < 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        T temp = this.items[index];
+        this.ShiftLeft(index);
+        this.Count--;
+        return temp;
+    }
+   
+
+    private void ShiftLeft(int index)
+    {
+        for (int i = index; i < this.Count; i++)
+        {
+            this.items[i] = this.items[i + 1];
+        }
+    }
+}
